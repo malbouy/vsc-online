5acfd2ae6b8d5360cdb35a2da74500adeb8d5ff8
Malbouy <75235131+malbouy@users.noreply.github.com>
2021-09-26 15:10:46 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
-if [[ ! -d code-server-3.9.3-linux-amd64 ]]
+if [[ ! -d code-server-3.12.0-linux-amd64 ]]
 then
     wget https://github.com/cdr/code-server/releases/download/v3.12.0/code-server-3.12.0-linux-amd64.tar.gz
-    tar xvzf code-server-3.9.3-linux-amd64.tar.gz
+    tar xvzf code-server-3.12.0-linux-amd64.tar.gz
 fi
 
-cd code-server-3.9.3-linux-amd64/bin
+cd code-server-3.12.0-linux-amd64.tar.gz
 
 # ./code-server --install-extension extension.id
 
